8da77fc5d39883e8037a524e9c85f3d8c21548e4
jackguo2016 <923081130@qq.com>
2022-11-17 15:50:37 -0500

    update

--- SupportVectorMachine.py
+++ SupportVectorMachine.py
@@ -8,6 +8,39 @@ from sklearn.utils import shuffle
 regularization_strength = 10000
 learning_rate = 0.000001
 
+print("reading dataset...")
+# read data in pandas (pd) data frame
+data = pd.read_csv('data.csv')
+
+# drop last column (extra column added by pd)
+# and unnecessary first column (id)
+data.drop(data.columns[[-1, 0]], axis=1, inplace=True)
+
+print("applying feature engineering...")
+# convert categorical labels to numbers
+diag_map = {'M': 1.0, 'B': -1.0}
+data['diagnosis'] = data['diagnosis'].map(diag_map)
+
+# put features & outputs in different data frames
+Y = data.loc[:, 'diagnosis']
+X = data.iloc[:, 1:]
+
+# # filter features
+# remove_correlated_features(X)
+# remove_less_significant_features(X, Y)
+
+# normalize data for better convergence and to prevent overflow
+X_normalized = MinMaxScaler().fit_transform(X.values)
+X = pd.DataFrame(X_normalized)
+
+# insert 1 in every row for intercept b
+X.insert(loc=len(X.columns), column='intercept', value=1)
+
+# split data into train and test set
+print("splitting dataset into train and test sets...")
+X_train, X_test, y_train, y_test = tts(X, Y, test_size=0.2, random_state=42)
+
+
 def svm(features, outputs):
     max_epochs = 5000
     weights = np.zeros(features.shape[1])
@@ -51,62 +84,26 @@ def svm(features, outputs):
     return weights
 
 
-def init():
-    print("reading dataset...")
-    # read data in pandas (pd) data frame
-    data = pd.read_csv('data.csv')
-
-    # drop last column (extra column added by pd)
-    # and unnecessary first column (id)
-    data.drop(data.columns[[-1, 0]], axis=1, inplace=True)
-
-    print("applying feature engineering...")
-    # convert categorical labels to numbers
-    diag_map = {'M': 1.0, 'B': -1.0}
-    data['diagnosis'] = data['diagnosis'].map(diag_map)
-
-    # put features & outputs in different data frames
-    Y = data.loc[:, 'diagnosis']
-    X = data.iloc[:, 1:]
-
-    # # filter features
-    # remove_correlated_features(X)
-    # remove_less_significant_features(X, Y)
-
-    # normalize data for better convergence and to prevent overflow
-    X_normalized = MinMaxScaler().fit_transform(X.values)
-    X = pd.DataFrame(X_normalized)
-
-    # insert 1 in every row for intercept b
-    X.insert(loc=len(X.columns), column='intercept', value=1)
-
-    # split data into train and test set
-    print("splitting dataset into train and test sets...")
-    X_train, X_test, y_train, y_test = tts(X, Y, test_size=0.2, random_state=42)
-
-    # train the model
-    print("training started...")
-    W = svm(X_train.to_numpy(), y_train.to_numpy())
-    print("training finished.")
-    print("weights are: {}".format(W))
-
-    # testing the model
-    print("testing the model...")
-    y_train_predicted = np.array([])
-    for i in range(X_train.shape[0]):
-        yp = np.sign(np.dot(X_train.to_numpy()[i], W))
-        y_train_predicted = np.append(y_train_predicted, yp)
-
-    y_test_predicted = np.array([])
-    for i in range(X_test.shape[0]):
-        yp = np.sign(np.dot(X_test.to_numpy()[i], W))
-        y_test_predicted = np.append(y_test_predicted, yp)
-
-    print("accuracy on test dataset: {}".format(accuracy_score(y_test, y_test_predicted)))
-    print("recall on test dataset: {}".format(recall_score(y_test, y_test_predicted)))
-    print("precision on test dataset: {}".format(recall_score(y_test, y_test_predicted)))
 
 
-# set hyper-parameters and call init
+# train the model
+print("training started...")
+W = svm(X_train.to_numpy(), y_train.to_numpy())
+print("training finished.")
+print("weights are: {}".format(W))
 
-init()
+# testing the model
+print("testing the model...")
+y_train_predicted = np.array([])
+for i in range(X_train.shape[0]):
+    yp = np.sign(np.dot(X_train.to_numpy()[i], W))
+    y_train_predicted = np.append(y_train_predicted, yp)
+
+y_test_predicted = np.array([])
+for i in range(X_test.shape[0]):
+    yp = np.sign(np.dot(X_test.to_numpy()[i], W))
+    y_test_predicted = np.append(y_test_predicted, yp)
+
+print("accuracy on test dataset: {}".format(accuracy_score(y_test, y_test_predicted)))
+print("recall on test dataset: {}".format(recall_score(y_test, y_test_predicted)))
+print("precision on test dataset: {}".format(recall_score(y_test, y_test_predicted)))
